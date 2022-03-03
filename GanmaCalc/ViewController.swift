a9d165357c7a5c76ccd376327379ce7262451c56
alpacadevapp <alpaca.dev.app@gmail.com>
2022-03-03 17:43:03 +0900

    calc

--- GanmaCalc/ViewController.swift
+++ GanmaCalc/ViewController.swift
@@ -8,12 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var weigthTextField: UITextField!
+    @IBOutlet weak var ganmaTextField: UITextField!
+    @IBOutlet weak var resultRyuryo: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        weigthTextField.placeholder = "Kg"
+        ganmaTextField.placeholder = "γ"
     }
 
+    @IBAction func ganmaCalc(_ sender: Any) {
+            let doubleW = Double(weigthTextField.text!)
+            let doubleG = Double(ganmaTextField.text!)
+        resultRyuryo.text = calculation(weight: doubleW!, ganma: doubleG!)
+        }
+    }
+    
+    func calculation(weight: Double, ganma: Double) -> String {
+        let w = weight
+        let g = ganma
+        var result = w + g
+        result = floor(result * 10) / 10
 
-}
+        return result.description
+    }
+    
+    
+
 
